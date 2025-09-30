fdf78bacde5c52ee2c451098e1d94441bce3093d
n3on <n3on@n3ons-MacBook-Pro.local>
2025-09-30 14:12:13 +0200

    Feat: Add accessibility page

---
+++ packages/web/src/app/accessibility/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react';
+
+const AccessibilityPage: React.FC = () => {
+  return (
+    <main className="container py-4">
+      <h1>Accessibility Settings</h1>
+      <p>This is the accessibility settings page. Here you can configure various accessibility options for the application.</p>
+      {/* Add more accessibility settings components here */}
+    </main>
+  );
+};
+
+export default AccessibilityPage;
